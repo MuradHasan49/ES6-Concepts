e29cec3be7a1aa8905986fb73c3878b63f022bec
Murad Hasan <muradhasanshuco00@gmail.com>
2026-03-01 11:41:41 +0600

    Explore ES6 String Litarel

---
+++ ES6 Concepts/string-litarel.js
@@ -0,0 +1,12 @@
+    
+    // Normal way 
+    const firstName = 'Murad '
+    const lastName = 'Hasan'
+
+    let fullName = firstName + "" + lastName;
+    console.log(fullName)
+    
+    // string litarel 
+    
+    let fullname2 = `${firstName}${lastName}`
+    console.log(fullname2)
