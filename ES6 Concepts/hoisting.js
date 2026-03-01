19edb477050f683f75905705b684c016d07c74c2
Murad Hasan <muradhasanshuco00@gmail.com>
2026-03-01 11:35:14 +0600

    testing hoisting

---
+++ ES6 Concepts/hoisting.js
@@ -0,0 +1,12 @@
+
+var hoisting = function(x,y){
+    console.log(x/y)
+} 
+hoisting(2,10)
+
+console.log(number1)
+var number1 = 23 
+let number2 = 23 
+const number3 = 23 
+console.log(number2)
+console.log(number3)
