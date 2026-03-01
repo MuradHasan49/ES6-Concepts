5b10270d41b4da3e7e16c46c0f7f9e6f5897a7cb
Murad Hasan <muradhasanshuco00@gmail.com>
2026-03-01 12:26:51 +0600

    recap loop

---
+++ ES6 Concepts/loop.js
@@ -0,0 +1,19 @@
+let ar = [1, 23, 4, 4, 5, 5, 676, 7]
+
+for (let i of ar) {
+    console.log(i)
+}
+
+
+
+const course = {
+    title: "Modern Web Development",
+    duration: "6 Months",
+    firstName: "Murad",
+    lastName: "Hasan",
+    status: "Active"
+};
+
+for (let j in course) {
+    console.log(course[j])
+}
